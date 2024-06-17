57f0ecdec68cb44d0b04c44e87409afd4537d198
NuboHeimer <NuboHeimer@yandex.ru>
2024-06-17 13:11:14 +0300

    Initial commit.

---
+++ GetMinichatSettings.cs
@@ -0,0 +1,7 @@
+///----------------------------------------------------------------------------
+///   Module:       Get minichat settings
+///   Author:       NuboHeimer (https://vkplay.live/nuboheimer)
+///   Email:        nuboheimer@yandex.ru
+///   Telegram:     t.me/nuboheimer
+///   Version:      0.0.0
+///----------------------------------------------------------------------------
