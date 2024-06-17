e905ddc5f210aa26bad83d4accc58fa76cfc9036
NuboHeimer <NuboHeimer@yandex.ru>
2024-06-17 14:09:43 +0300

    Добавил получение данных твича.

--- GetMinichatSettings.cs
+++ GetMinichatSettings.cs
@@ -5,3 +5,61 @@
 ///   Telegram:     t.me/nuboheimer
 ///   Version:      0.0.0
 ///----------------------------------------------------------------------------
+
+using System;
+using System.IO;
+using Newtonsoft.Json;
+
+public class CPHInline
+{
+    public string GetAllSettings()
+    {
+        string miniChatSettingsPath = args["miniChatSettingsPath"].ToString();
+        string miniChatSettings = File.ReadAllText(miniChatSettingsPath);
+        return miniChatSettings;
+    }
+
+    public bool GetTwitchChannelName()
+    {
+        var data = JsonConvert.DeserializeObject<MiniChatSettings>(GetAllSettings());
+        CPH.SetArgument("twitchChannelName", data.Twitch.ChannelName);
+        return true;
+    }
+    public bool GetTwitchAccessToken()
+    {
+        var data = JsonConvert.DeserializeObject<MiniChatSettings>(GetAllSettings());
+        CPH.SetArgument("twitchAccessToken", data.Twitch.AccessToken);
+        return true;
+    }
+    public bool GetTwitchUserID()
+    {
+        var data = JsonConvert.DeserializeObject<MiniChatSettings>(GetAllSettings());
+        CPH.SetArgument("twitchUserID", data.Twitch.UserID);
+        return true;
+    }
+    public bool GetTwitchLogin()
+    {
+        var data = JsonConvert.DeserializeObject<MiniChatSettings>(GetAllSettings());
+        CPH.SetArgument("twitchLogin", data.Twitch.Login);
+        return true;
+    }
+
+    public class MiniChatSettings
+    {
+        public TwitchData Twitch { get; set; }
+        public GoodGameData GoodGame { get; set; }
+    }
+
+    public class TwitchData
+    {
+        public string ChannelName { get; set; }
+        public string AccessToken { get; set; }
+        public string UserID { get; set; }
+        public string Login { get; set; }
+    }
+    public class GoodGameData
+    {
+        public string ChannelKey { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
