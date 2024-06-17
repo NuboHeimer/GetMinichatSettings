8732d6582943b4d1e3b3cad7d899e8ca4815c05d
NuboHeimer <NuboHeimer@yandex.ru>
2024-06-17 15:25:14 +0300

    Добавил бусти, плюс правки стиля.

--- GetMinichatSettings.cs
+++ GetMinichatSettings.cs
@@ -3,7 +3,7 @@
 ///   Author:       NuboHeimer (https://vkplay.live/nuboheimer)
 ///   Email:        nuboheimer@yandex.ru
 ///   Telegram:     t.me/nuboheimer
-///   Version:      0.5.0
+///   Version:      0.6.0
 ///----------------------------------------------------------------------------
 
 using System;
@@ -106,25 +106,61 @@ public class CPHInline
     public bool GetVKPlayBlogUrl()
     {
         var data = JsonConvert.DeserializeObject<MiniChatSettings>(GetAllSettings());
-        CPH.SetArgument("VKPlayBlogUrl", data.VKPlay.BlogUrl);
+        CPH.SetArgument("vkPlayBlogUrl", data.VKPlay.BlogUrl);
         return true;
     }
     public bool GetVKPlayClientID()
     {
         var data = JsonConvert.DeserializeObject<MiniChatSettings>(GetAllSettings());
-        CPH.SetArgument("VKPlayClientID", data.VKPlay.ClientID);
+        CPH.SetArgument("vkPlayClientID", data.VKPlay.ClientID);
         return true;
     }
     public bool GetVKPlayAccessToken()
     {
         var data = JsonConvert.DeserializeObject<MiniChatSettings>(GetAllSettings());
-        CPH.SetArgument("VKPlayAccessToken", data.VKPlay.AccessToken);
+        CPH.SetArgument("vkPlayAccessToken", data.VKPlay.AccessToken);
+        return true;
+    }
+    public bool GetVKPlayRefreshToken()
+    {
+        var data = JsonConvert.DeserializeObject<MiniChatSettings>(GetAllSettings());
+        CPH.SetArgument("vkPlayRefreshToken", data.VKPlay.RefreshToken);
         return true;
     }
     public bool GetVKPlayExpiresAt()
     {
         var data = JsonConvert.DeserializeObject<MiniChatSettings>(GetAllSettings());
-        CPH.SetArgument("VKPlayExpiresAt", data.VKPlay.ExpiresAt);
+        CPH.SetArgument("vkPlayExpiresAt", data.VKPlay.ExpiresAt);
+        return true;
+    }
+    public bool GetBoostyBlogUrl()
+    {
+        var data = JsonConvert.DeserializeObject<MiniChatSettings>(GetAllSettings());
+        CPH.SetArgument("boostyBlogUrl", data.Boosty.BlogUrl);
+        return true;
+    }
+    public bool GetBoostyClientID()
+    {
+        var data = JsonConvert.DeserializeObject<MiniChatSettings>(GetAllSettings());
+        CPH.SetArgument("boostyClientID", data.Boosty.ClientID);
+        return true;
+    }
+    public bool GetBoostyAccessToken()
+    {
+        var data = JsonConvert.DeserializeObject<MiniChatSettings>(GetAllSettings());
+        CPH.SetArgument("boostyAccessToken", data.Boosty.AccessToken);
+        return true;
+    }
+    public bool GetBoostyRefreshToken()
+    {
+        var data = JsonConvert.DeserializeObject<MiniChatSettings>(GetAllSettings());
+        CPH.SetArgument("boostyRefreshToken", data.Boosty.RefreshToken);
+        return true;
+    }
+    public bool GetBoostyExpiresAt()
+    {
+        var data = JsonConvert.DeserializeObject<MiniChatSettings>(GetAllSettings());
+        CPH.SetArgument("boostyExpiresAt", data.Boosty.ExpiresAt);
         return true;
     }
 
@@ -135,6 +171,7 @@ public class CPHInline
         public GoodGameData GoodGame { get; set; }
         public string DonationAlertsToken { get; set; }
         public VKPlayData VKPlay { get; set; }
+        public BoostyData Boosty { get; set; }
     }
 
     public class TwitchData
@@ -160,6 +197,14 @@ public class CPHInline
         public string UserID { get; set; }
     }
     public class VKPlayData
+    {
+        public string BlogUrl { get; set; }
+        public string ClientID { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public string ExpiresAt { get; set; }
+    }
+    public class BoostyData
     {
         public string BlogUrl { get; set; }
         public string ClientID { get; set; }
